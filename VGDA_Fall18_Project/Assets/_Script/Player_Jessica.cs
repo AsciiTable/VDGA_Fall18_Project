53ac497a1525d43d8385f1e83dbdfd3d0ed31cfc
Jessica Wei <jessicawei9@gmail.com>
2018-09-30 20:25:02 -0700

    added horizontal movement and flipping to Steve in Player_Jessica

--- VGDA_Fall18_Project/Assets/_Script/Player_Jessica.cs
+++ VGDA_Fall18_Project/Assets/_Script/Player_Jessica.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class Player_Jessica : MonoBehaviour {
     public int MAXHP = 10;
     public int CURRENTHP;
+    public float moveSpeed = 5f;
     // Use this for initialization
     void Start () {
 		
 	}
 	
 	// Update is called once per frame
+    /*Includes:
+     * Special Ghose Power CD
+     * Recieving input to: jump, move left, move right, swing bat, apologize, run
+     */
 	void Update () {
-		//Special Ghost Power CD
-        //Recieving input to:  jump, move left, move right, swing bat, apologize, run
-	}
+        //Horizontal movement w/ flip
+        float inputHorizontal = Input.GetAxis("Horizontal");
+        transform.position = transform.position + new Vector3((inputHorizontal * moveSpeed * Time.deltaTime), 0, 0);
+        if(inputHorizontal > 0 && GetComponent<SpriteRenderer>().flipX){
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else if (inputHorizontal < 0 && !GetComponent<SpriteRenderer>().flipX){
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+    }
 }
