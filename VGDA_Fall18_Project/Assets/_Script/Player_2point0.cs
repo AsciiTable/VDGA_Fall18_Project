f610b7b8bedde1d4aa92d24916d0c4c54c616970
jusin-PC\Just <justincheung6@gmail.com>
2018-10-03 16:04:29 -0700

    Modified/fixed double jump; increased Player_2.0 gravity

--- VGDA_Fall18_Project/Assets/_Script/Player_2point0.cs
+++ VGDA_Fall18_Project/Assets/_Script/Player_2point0.cs
@@ -4,26 +4,25 @@ using UnityEngine;
 
 public class Player_2point0 : MonoBehaviour {
 
-    public float Player_moveForce = 0.5f;
-    public float Player_jumpForce = 500f;
-    public float Player_jumpForceMax = 10f;
-    private int Player_jumpsMax = 2;
+    public float Player_moveForce = 0.4f;
+    public float Player_jumpForce = 40f;
+    public float Player_doubleJumpForce = 30f;
+    public float Player_gravity = 10f;
+    public int Player_jumpsMax = 2;
 
-    private int Player_jumps = 0;
-    private bool Player_flipped = false;
-    private bool Player_grounded = false;
+    [SerializeField] private int Player_jumps = 0;
+    [SerializeField] private bool Player_flipped = false;
+    [SerializeField] private bool Player_groundCheck = false;
 
 
 
     private Rigidbody2D Player_rb2d;
-    private Collider2D Player_col2D;
     private Transform groundCheck1_transform;
     private Transform groundCheck2_transform;
 
     void Awake()
     {
         Player_rb2d = GetComponent<Rigidbody2D>();
-        Player_col2D = GetComponent<Collider2D>();
 
         groundCheck1_transform = transform.Find("groundCheck_1");
         groundCheck2_transform = transform.Find("groundCheck_2");
@@ -32,43 +31,48 @@ public class Player_2point0 : MonoBehaviour {
 
     void FixedUpdate()
     {
-
+        Player_rb2d.gravityScale = Player_gravity;
         float Player_horizontal = Input.GetAxis("Horizontal");
 
         //move player left and right
-        Player_rb2d.transform.Translate(Player_horizontal * Player_moveForce, 0f, 0f);
+        Player_rb2d.transform.Translate((Player_horizontal * Player_moveForce), 0f, 0f);
 
         //--------------------------------------------------------------------------------------------------------------------------------
-
-        //Check if on floor
-        Player_grounded = Physics2D.Linecast(groundCheck1_transform.position, groundCheck2_transform.position, 1 << LayerMask.NameToLayer("Platforms"));
-
-        if(Player_grounded)
-        {
-                //Debug.Log("Grounded");
-            Player_jumps = 0;
-        }
-
-        //Jump in air
-        if (Input.GetButtonDown("Jump") && Player_jumps < Player_jumpsMax && Player_jumps > 0)
-        {
-            Debug.Log("Double Lifted");
-            Player_rb2d.AddForce(new Vector2(0f, Player_jumpForce)); // NOTE: Change this to make it less powerful
-            Player_jumps++;
-        }
-        //Jump off floor
-        if (Input.GetButtonDown("Jump") && Player_jumps == 0)
-        {
-            Debug.Log("Lifted");
-            Player_rb2d.AddForce(new Vector2(0f, Player_jumpForce));
-            Player_jumps++;
-        }
-
-
 
     }
 
     void Update () {
-		
-	}
+        //Check if on floor
+        Player_groundCheck = Physics2D.Linecast(groundCheck1_transform.position, groundCheck2_transform.position, 1 << LayerMask.NameToLayer("Platforms"));
+
+        if (Player_groundCheck)
+        {
+            //Debug.Log("Grounded");
+            Player_jumps = 0;
+        }
+
+        //Jump in air
+        if (Input.GetButtonDown("Jump") && Player_jumps != Player_jumpsMax && Player_jumps != 0 && !(Player_groundCheck))
+        {
+            Debug.Log("Double Lifted");
+            Player_jumps++;
+            if (Player_rb2d.velocity.x < Player_doubleJumpForce)
+            {
+                Player_rb2d.velocity = new Vector3(0f, Player_doubleJumpForce, 0f);
+            }
+        }
+        //Jump off floor
+        else if (Input.GetButtonDown("Jump") && Player_jumps == 0 && Player_groundCheck)
+        {
+            Debug.Log("Lifted");
+            Player_rb2d.velocity = new Vector3(0f, Player_jumpForce, 0f);
+            Player_jumps++;
+        }
+        //Cancels first jump if in air
+        else if (!(Player_groundCheck) && Player_jumps == 0)
+        {
+            Debug.Log("Unlifted");
+            Player_jumps++;
+        }
+    }
 }
