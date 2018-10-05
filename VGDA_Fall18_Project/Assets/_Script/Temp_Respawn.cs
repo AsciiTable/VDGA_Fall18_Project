7ebf770bfaa88bd680c254c03e060a27850fb7f6
Jessica Wei <jessicawei9@gmail.com>
2018-10-05 14:15:03 -0700

    put respawn script in, Just's code scene doesn't like it somehow

---
+++ VGDA_Fall18_Project/Assets/_Script/Temp_Respawn.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Temp_Respawn : MonoBehaviour {
+
+    public GameObject spawnPosition;
+    public float verticalPos = -7.35f;
+    public float horizontalPos = -2.93f;
+    private bool isDead = false;
+    public float SuicidePoint = -15f;
+    public Transform playerTrans;
+    public GameObject deathSpace;
+
+    // Use this for initialization
+    void Start () {
+        isDead = false;
+    }
+	
+    // Update is called once per frame
+    void Update () {
+        if (isDead == true || playerTrans.position.y < SuicidePoint){
+            Debug.Log("some death is true");
+            ResetScene();
+        }
+    }
+    //tbh is not used but we'll see
+    public void Die(){
+        Debug.Log("is dead");
+        isDead = true;
+
+    }
+
+    //Resets the scene to the beginning
+    public void ResetScene(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
