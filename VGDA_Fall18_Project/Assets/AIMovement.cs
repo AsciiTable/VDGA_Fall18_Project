23b371206f21f2f2455b3b9ee4ad7be959fedcf5
Jessica Wei <jessicawei9@gmail.com>
2018-10-07 19:49:46 -0700

    AI now moves on its own and can flip when necessary

--- VGDA_Fall18_Project/Assets/AIMovement.cs
+++ VGDA_Fall18_Project/Assets/AIMovement.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class AIMovement : MonoBehaviour {
 	public float MoveSpeed = 5f;
+	private Rigidbody2D _enemy;
 	// Use this for initialization
 	void Start () {
-		
+		_enemy = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		transform.position = transform.position + new Vector3((MoveSpeed * Time.deltaTime), 0, 0);
+		if(transform.position.x < 0 && GetComponent<SpriteRenderer>().flipX){
+			GetComponent<SpriteRenderer>().flipX = false;
+		}
+		else if (transform.position.x> 0 && !GetComponent<SpriteRenderer>().flipX){
+			GetComponent<SpriteRenderer>().flipX = true;
+		}
 		
 	}
 }
