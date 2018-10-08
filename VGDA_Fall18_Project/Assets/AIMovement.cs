47784210895fcc71072c2d8ca1cd3657bb25138c
Jessica Wei <jessicawei9@gmail.com>
2018-10-07 20:12:47 -0700

    Added check on the sprite and will move in the direction only if it is physically facing that direction

--- VGDA_Fall18_Project/Assets/AIMovement.cs
+++ VGDA_Fall18_Project/Assets/AIMovement.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class AIMovement : MonoBehaviour {
 	public float MoveSpeed = 5f;
 	private Rigidbody2D _enemy;
+
+	public bool SpriteFacingRight;//asks for what side the sprite is facing before 
 	// Use this for initialization
 	void Start () {
 		_enemy = GetComponent<Rigidbody2D>();
@@ -12,13 +16,26 @@ public class AIMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = transform.position + new Vector3((MoveSpeed * Time.deltaTime), 0, 0);
-		if(transform.position.x < 0 && GetComponent<SpriteRenderer>().flipX){
-			GetComponent<SpriteRenderer>().flipX = false;
+		if (SpriteFacingRight == true){
+			transform.position = transform.position + new Vector3((MoveSpeed * Time.deltaTime), 0, 0);
+			if(transform.position.x > 0 && GetComponent<SpriteRenderer>().flipX){
+				GetComponent<SpriteRenderer>().flipX = false;
+			}
+			else if (transform.position.x < 0 && !GetComponent<SpriteRenderer>().flipX){
+				GetComponent<SpriteRenderer>().flipX = true;
+			}
 		}
-		else if (transform.position.x> 0 && !GetComponent<SpriteRenderer>().flipX){
-			GetComponent<SpriteRenderer>().flipX = true;
+		else{
+			transform.position = transform.position + new Vector3((-1*MoveSpeed * Time.deltaTime), 0, 0);
+			if(transform.position.x > 0 && GetComponent<SpriteRenderer>().flipX){
+				GetComponent<SpriteRenderer>().flipX = true;
+			}
+			else if (transform.position.x < 0 && !GetComponent<SpriteRenderer>().flipX){
+				GetComponent<SpriteRenderer>().flipX = false;
+			}
 		}
+
+
 		
 	}
 }
