33043208792b2b606008504ce1df776d3ab2a1db
Jessica Wei <jessicawei9@gmail.com>
2018-10-07 21:23:33 -0700

    I need a second to think about AI turning

--- VGDA_Fall18_Project/Assets/AIMovement.cs
+++ VGDA_Fall18_Project/Assets/AIMovement.cs
@@ -17,18 +17,33 @@ public class AIMovement : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update(){
+	void Update()
+	{
+		if (HasTurned()){
+		}
+
 		Move(MovingRight());
 	}
 
-	public int MovingRight(){//returns 1 if AI is moving right
-		if (SpriteFacingRight == true){
+	int MovingRight(){//returns 1 if AI is moving right
+		if (SpriteFacingRight == true && GetComponent<SpriteRenderer>().flipX == false){
+			Debug.Log("Moving right!");
 			return 1;
 		}
+		Debug.Log("Moving left!");
 		return -1;
 	}
 	
-	void Move(int right){
-		transform.position = transform.position + new Vector3(right*MoveSpeed*Time.deltaTime,0,0);
+	void Move(int direction){
+		transform.position = transform.position + new Vector3(direction*MoveSpeed*Time.deltaTime,0,0);
+	}
+
+	bool HasTurned(){
+		if (!SpriteFacingRight){
+			if (_enemy.position.x < MaxLeftPos){
+				GetComponent<SpriteRenderer>().flipX = true;
+				return true;
+			}
+		}
 	}
 }
