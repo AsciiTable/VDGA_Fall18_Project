ad08961062c34e76514e03ec556a899bdb7f81bf
Jessica Wei <jessicawei9@gmail.com>
2018-10-14 19:56:42 -0700

    Added knockback onto enemy

---
+++ VGDA_Fall18_Project/Assets/AI_Knockback.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Knockback : MonoBehaviour{
+	
+	public bool isHit = false;
+	public float knockBack = 500f;
+	private Rigidbody2D enemy;
+	
+	// Use this for initialization
+	void Start (){
+		enemy = GetComponent<Rigidbody2D>();
+	}
+
+	private void FixedUpdate(){
+		if (isHit){
+			enemy.AddForce(transform.right*knockBack);
+			isHit = false;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
