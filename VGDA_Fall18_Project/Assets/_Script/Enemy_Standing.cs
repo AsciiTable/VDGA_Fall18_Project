9936ff3a745e0eebf061356c2386bb6b4d8ea6d3
jusin-PC\Just <justincheung6@gmail.com>
2018-10-30 19:49:27 -0700

    Patched up Prefabs

---
+++ VGDA_Fall18_Project/Assets/_Script/Enemy_Standing.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Standing : MonoBehaviour
+{
+
+    private Player Player_script;
+
+    private void Awake()
+    {
+        Player_script = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("Enemy Death");
+            Player_script.ResetScene();
+        }
+    }
+
+}
