7a9e6647014d480051e0c33f25c5a70d3eee75f6
jusin-PC\Just <justincheung6@gmail.com>
2018-11-02 13:41:42 -0700

    Patched up Enemy Moving and Standing Scripts so they work as intended.

--- VGDA_Fall18_Project/Assets/_Script/Enemy_Standing.cs
+++ VGDA_Fall18_Project/Assets/_Script/Enemy_Standing.cs
@@ -7,12 +7,31 @@ public class Enemy_Standing : MonoBehaviour
 
     private Player Player_script;
 
+    private BoxCollider2D Enemy_Standing_collider;
+
+    private bool Player_invulnerable_get;
+
     private void Awake()
     {
+        Enemy_Standing_collider = GetComponent<BoxCollider2D>();
         Player_script = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
     }
 
-    void OnCollisionEnter2D(Collision2D col)
+    private void Update()
+    {
+        Player_invulnerable_get = Player_script.Player_invulnerable;
+
+        if (Player_invulnerable_get == true)
+        {
+            Enemy_Standing_collider.isTrigger = true;
+        }
+        if (Player_invulnerable_get == false)
+        {
+            Enemy_Standing_collider.isTrigger = false;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D col)
     {
         if (col.gameObject.tag == "Player")
         {
@@ -20,5 +39,4 @@ public class Enemy_Standing : MonoBehaviour
             Player_script.ResetScene();
         }
     }
-
 }
