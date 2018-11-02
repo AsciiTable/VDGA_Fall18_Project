2e04fc94a0033cdcefa03d248592c5e5d0229b5b
Jessica Wei <jessicawei9@gmail.com>
2018-11-02 13:40:14 -0700

    changed input keys for inv and jump to x and up key respectively

--- VGDA_Fall18_Project/Assets/AI_Despawn.cs
+++ VGDA_Fall18_Project/Assets/AI_Despawn.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AI_Despawn : MonoBehaviour {
+public class AI_Despawn : MonoBehaviour{
 
+	private GameObject enemy;
+	public float YDespawnPosition = -15f;
+	public float XDespawnPosition = -1500f;
+
+	private float enemyYPosition;
+	private float enemyXPosition;
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		enemyYPosition = enemy.GetComponent<SpriteRenderer>().transform.position.y;
+		enemyXPosition = enemy.GetComponent<SpriteRenderer>().transform.position.x;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		enemyYPosition = enemy.GetComponent<SpriteRenderer>().transform.position.y;
+		enemyXPosition = enemy.GetComponent<SpriteRenderer>().transform.position.x;
+
+		if (enemyYPosition < YDespawnPosition || enemyYPosition < XDespawnPosition)
+		{
+			Destroy(enemy);
+		}
+
 	}
 }
