c70c6aed8d213f0bb8885a19de3b909b69e16949
jusin-PC\Just <justincheung6@gmail.com>
2018-11-19 11:23:27 -0800

    Added Collectables and Timer to Level 1.1

---
+++ VGDA_Fall18_Project/Assets/_Script/Collectable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour {
+
+    private Timer_Controller Timer_Script;
+
+	void Start () {
+        GameObject gameObject_ScriptHolder = GameObject.Find("ScriptHolder");
+        Timer_Script = gameObject_ScriptHolder.GetComponent<Timer_Controller>();
+	}
+	
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+            Timer_Script.CollectTime();
+        }
+    }
+}
---
+++ VGDA_Fall18_Project/Assets/_Script/Timer_Controller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer_Controller : MonoBehaviour {
+
+    [Header("Editable")]
+    public int timeMax;
+    public int timeCollect;
+
+    [Space(10)]
+
+    [Header("Resets by Start Command")]
+    [Tooltip("Starts as timeMax")]
+    public int timeLeft;
+
+
+    void Start () {
+
+        timeLeft = timeMax;
+
+        //Start Every one Second
+        InvokeRepeating("TimerPass", 0, 1);
+	}
+
+    private void TimerPass()
+    {
+        timeLeft -= 1;
+        Debug.Log(timeLeft);
+
+        if (timeLeft <= 0)
+        {
+            Debug.Log("TIMESUP");
+            CancelInvoke();
+        }
+    }
+
+    public void CollectTime()
+    {
+        timeLeft += timeCollect;
+        if (timeLeft > timeMax)
+        {
+            timeLeft = timeMax;
+        }
+    }
+
+}
