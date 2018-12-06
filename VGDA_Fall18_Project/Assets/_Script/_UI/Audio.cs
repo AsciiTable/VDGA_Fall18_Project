e82f733625fe586a776dd00dfe88d86fcd76bc98
Jessica Wei <jessicawei9@gmail.com>
2018-12-06 06:05:15 -0800

    Initialized audio script

---
+++ VGDA_Fall18_Project/Assets/_Script/_UI/Audio.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+
+	public static AudioClip WalkSFX;
+	public static AudioClip ThudSFX;
+	private static AudioSrouce MusicSource;
+	
+
+	// Use this for initialization
+	void Start ()
+	{
+		MusicSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	pubic static void PlaySound(string clip)
+	{
+		switch (clip)
+		{
+			default:
+				break;
+		}
+	}
+}
