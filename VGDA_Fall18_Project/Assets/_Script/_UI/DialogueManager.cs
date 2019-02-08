54fbf96d3911951d48e2cd35abb5f98e123cbb23
Jessica Wei <jessicawei9@gmail.com>
2019-02-08 15:06:08 -0800

    Dialogue box works in console

--- VGDA_Fall18_Project/Assets/_Script/_UI/DialogueManager.cs
+++ VGDA_Fall18_Project/Assets/_Script/_UI/DialogueManager.cs
@@ -12,4 +12,34 @@ public class DialogueManager : MonoBehaviour
         sentences = new Queue<string>();
     }
 
+    public void StartDialogue(Dialogue dialogue) {
+        Debug.Log("Starting conversation with " + dialogue.name);
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence() {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        else {
+            string sentence = sentences.Dequeue();
+            Debug.Log(sentence);
+        }
+
+    }
+
+    public void EndDialogue() {
+        Debug.Log("End of Conversation");
+    }
+
 }
