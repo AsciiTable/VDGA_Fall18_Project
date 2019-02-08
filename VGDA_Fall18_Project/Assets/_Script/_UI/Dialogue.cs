e8bafba4d8bb5e9be9ebf10df1c84920335c45ce
Jessica Wei <jessicawei9@gmail.com>
2019-02-08 14:34:49 -0800

    lots of dialogue scripts

---
+++ VGDA_Fall18_Project/Assets/_Script/_UI/Dialogue.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Allows the functions to be edited inside of the engine
+[System.Serializable]
+public class Dialogue
+{
+    //Name of the speaker
+    public string name;
+
+    [TextArea(3, 10)]
+    //Array of sentences
+    public string[] sentences;
+}
