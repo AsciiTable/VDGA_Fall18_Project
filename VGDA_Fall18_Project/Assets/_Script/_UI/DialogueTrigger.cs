74de678f07d5e734705a1429e345b526d4e2377a
Jessica Wei <jessicawei9@gmail.com>
2019-02-15 14:59:28 -0800

    I'm testing the interact w/ dialogue box rn -- took movement off of a and d

--- VGDA_Fall18_Project/Assets/_Script/_UI/DialogueTrigger.cs
+++ VGDA_Fall18_Project/Assets/_Script/_UI/DialogueTrigger.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
-
+    
     public void TriggerDialogue() {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
     }
 }
