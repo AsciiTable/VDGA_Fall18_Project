79d24ff76769c9579e2b7af3fe0ad9ee26897491
Jessica Wei <jessicawei9@gmail.com>
2019-02-22 14:53:26 -0800

    DialgoueTrigger script cleaned up and commented

--- VGDA_Fall18_Project/Assets/_Script/_UI/DialogueTrigger.cs
+++ VGDA_Fall18_Project/Assets/_Script/_UI/DialogueTrigger.cs
@@ -4,31 +4,26 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    public GameObject NPC;
-    public GameObject Player;
-    public float InteractDistance = 5f;
-    public Dialogue dialogue;
+    public GameObject NPC; // the NPC who talks
+    public GameObject Player; // the Player who talks to the NPC
+    public float InteractDistance = 5f; // The acceptable distance in which the player can start talking to the NPC
+    public Dialogue dialogue; // the Dialogue spoken by the NPC
 
-
-    private void Start()
-    {
-        NPC.GetComponent<BoxCollider2D>().enabled = true;
-    }
     private void FixedUpdate()
     {
         float npcPosition = NPC.transform.position.x;
         float playerPosition = Player.transform.position.x;
         float distanceBetween = Mathf.Abs(npcPosition - playerPosition);
-        Debug.Log("Distance Between Player and NPC = " + distanceBetween);
+        //Debug.Log("Distance Between Player and NPC = " + distanceBetween);
         if (Input.GetButtonDown("InteractNPC") && (distanceBetween < InteractDistance))
-        {
-            NPC.GetComponent<BoxCollider2D>().enabled = true;
-            Debug.Log("Button a is pressed");
-            TriggerDialogue();
+        {// works if and only if player intends to talk to npc and they're close enough
+            //Debug.Log("Button a is pressed");
+            TriggerDialogue(); // Trigger Dialogue spoken
         }
     }
-
-    public void TriggerDialogue() {
+    // Triggers the dialogue through the Dialogue Manager
+    // Passes in the dialogue object for the function to display
+    public void TriggerDialogue() {  
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
 }
