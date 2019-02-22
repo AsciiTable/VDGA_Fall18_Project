177529e69389e53d3253198b472eb95f69364088
Jessica Wei <jessicawei9@gmail.com>
2019-02-22 14:46:33 -0800

    Interact distance now works and Dakota can adjust the distance however he likes

--- VGDA_Fall18_Project/Assets/_Script/_UI/DialogueTrigger.cs
+++ VGDA_Fall18_Project/Assets/_Script/_UI/DialogueTrigger.cs
@@ -4,35 +4,25 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
+    public GameObject NPC;
+    public GameObject Player;
+    public float InteractDistance = 5f;
     public Dialogue dialogue;
-    public GameObject npc;
-    public GameObject player;
 
-    private float distanceBetween;
-
-    /**private float playerPosition;
-    private float npcPosition;
-    private float distanceBetween;**/
 
     private void Start()
     {
-        npc.GetComponent<BoxCollider2D>().enabled = true;
-    }
-
-    private void Update()
-    {
-        float npcPosition = npc.transform.position.x;
-        float playerPosition = player.transform.position.x;
-        Debug.Log("X Position of Player = " + playerPosition);
-        float distanceBetween = Mathf.Abs(npcPosition - playerPosition);
-        Debug.Log("Distance Between Player and NPC = " + distanceBetween);
+        NPC.GetComponent<BoxCollider2D>().enabled = true;
     }
     private void FixedUpdate()
     {
-        
-        if (Input.GetButtonDown("InteractNPC") && (distanceBetween < 10))
+        float npcPosition = NPC.transform.position.x;
+        float playerPosition = Player.transform.position.x;
+        float distanceBetween = Mathf.Abs(npcPosition - playerPosition);
+        Debug.Log("Distance Between Player and NPC = " + distanceBetween);
+        if (Input.GetButtonDown("InteractNPC") && (distanceBetween < InteractDistance))
         {
-            npc.GetComponent<BoxCollider2D>().enabled = true;
+            NPC.GetComponent<BoxCollider2D>().enabled = true;
             Debug.Log("Button a is pressed");
             TriggerDialogue();
         }
@@ -41,20 +31,4 @@ public class DialogueTrigger : MonoBehaviour
     public void TriggerDialogue() {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
-
-    /**private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log("Function works");
-        
-        if (collision.CompareTag("Player"))
-        {
-            TriggerDialogue();
-            //npc.enabled = false;
-            //time needs to freeze when talking to NPC
-        }
-        else {
-            //npc.enabled = false;
-        }
-        
-    }**/
 }
