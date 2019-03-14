2bb6689dac934de75f1d7b90c61bc4e52374a785
jusin-PC\Just <justincheung6@gmail.com>
2019-03-14 14:04:10 -0700

    Finished most of Enemy_shoot sprite, only need to make sprite get stunned

---
+++ VGDA_Fall18_Project/Assets/_Script/_Enemies/Enemy_Projectile.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Projectile : MonoBehaviour
+{
+    [HideInInspector] public float direction;
+    [HideInInspector] public float speed;
+    [HideInInspector] public float startX;
+    [HideInInspector] public float distanceMax;
+
+    private Rigidbody2D Projectile_rb2d;
+    private Transform Projectile_xyz;
+
+    private Player Player_script;
+    private SpriteRenderer Player_sprite;
+
+    void Start()
+    {
+        Player_script = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        Player_sprite = GameObject.FindGameObjectWithTag("Player").GetComponent<SpriteRenderer>();
+        Projectile_rb2d = GetComponent<Rigidbody2D>();
+        Projectile_xyz = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        //Set speed of projectile
+        Projectile_rb2d.velocity = new Vector2(direction * Mathf.Abs(speed), 0f);
+
+        //Destroy projectile when it's out of range
+        if(Projectile_xyz.position.x < (startX - distanceMax) || Projectile_xyz.position.x > (startX + distanceMax))
+        {
+            Destroy(gameObject);
+            Debug.Log("Destroy");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            Debug.Log("Projectile Death");
+            Player_script.ResetScene();
+        }
+        if(col.gameObject.tag == "Bat")
+        {
+            speed = 30;
+            //Facing left
+            if (Player_sprite.flipX)
+            {
+                direction = -Mathf.Abs(direction);
+            }
+            //Facing right
+            if (!Player_sprite.flipX)
+            {
+                direction = Mathf.Abs(direction);
+            }
+        }
+    }
+}
--- VGDA_Fall18_Project/Assets/_Script/_Enemies/Enemy_Shoot.cs
+++ VGDA_Fall18_Project/Assets/_Script/_Enemies/Enemy_Shoot.cs
@@ -6,12 +6,19 @@ public class Enemy_Shoot : MonoBehaviour
 {
     //Variables
     [SerializeField] private int fireDelay;
+    [SerializeField] private int projectileSpeed;
+    [SerializeField] private int projDistanceMAX;
+    [SerializeField] private Vector3 projChangePosition;
+    private Vector3 projectilePosition;
 
     //Components
     private SpriteRenderer Enemy_sprite;
     private Transform Enemy_xyz;
+    private Rigidbody2D Enemy_rb2d;
 
-    //Components from Other Scripts
+    //Components from Other Objects
+    [SerializeField] private GameObject projectile;
+    private Enemy_Projectile Projectile_script;
     private Player Player_script;
     private Transform Player_xyz;
 
@@ -19,8 +26,10 @@ public class Enemy_Shoot : MonoBehaviour
     {
         Player_script = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         Player_xyz = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        Projectile_script = projectile.GetComponent<Enemy_Projectile>();
         Enemy_sprite = GetComponent<SpriteRenderer>();
         Enemy_xyz = GetComponent<Transform>();
+        Enemy_rb2d = GetComponent<Rigidbody2D>();
 
         //Activate IEnumerator fireShot()
         StartCoroutine(fireShot());
@@ -29,16 +38,28 @@ public class Enemy_Shoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Set projectile speed and max distance
+        Projectile_script.speed = Mathf.Abs(projectileSpeed);
+        Projectile_script.distanceMax = Mathf.Abs(projDistanceMAX);
+        Projectile_script.startX = projectilePosition.x;
+
        //Face left
        if(Enemy_xyz.position.x > Player_xyz.position.x)
         {
             Enemy_sprite.flipX = false;
+            Projectile_script.direction = -1f;
+            //Change Spawn Position of Bullet
+            projectilePosition.x = Enemy_xyz.position.x - projChangePosition.x;
+            projectilePosition.y = Enemy_xyz.position.y + projChangePosition.y;
+            projectilePosition.z = Enemy_xyz.position.z + projChangePosition.z;
         }
        //Face right
        else if (Enemy_xyz.position.x < Player_xyz.position.x)
         {
             Enemy_sprite.flipX = true;
+            Projectile_script.direction = 1f;
+            //Change Spawn Position of Bullet
+            projectilePosition = Enemy_xyz.position + projChangePosition;
         }
     }
 
@@ -48,7 +69,7 @@ public class Enemy_Shoot : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(fireDelay);
-            Debug.Log("Fire");
+            Instantiate(projectile, projectilePosition,Quaternion.identity);
         }
         
 
