358a289b2d88c0d8c09dd6fbd55103a818c8ad01
jusin-PC\Just <justincheung6@gmail.com>
2019-04-06 16:00:57 -0700

    Parker Can talk to Himself now

---
+++ VGDA_Fall18_Project/Assets/_Script/_UI/PlayerThoughts.cs
@@ -0,0 +1,43 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerThoughts : MonoBehaviour
+{
+
+    [SerializeField] private Dialogue thoughts;
+    [SerializeField] private GameObject Player;
+    public float InteractDistance = 5f;
+
+    private Transform xyz;
+    private DialogueTriggerPlayer script;
+    
+
+    private void Awake()
+    {
+        xyz = gameObject.GetComponent<Transform>();
+        script = Player.GetComponent<DialogueTriggerPlayer>();   
+    }
+    private void FixedUpdate()
+    {
+        float npcPosition = xyz.position.x;
+        float playerPosition = Player.transform.position.x;
+        float distanceBetween = Mathf.Abs(npcPosition - playerPosition);
+        if (Input.GetButtonDown("InteractNPC") && (distanceBetween < InteractDistance))
+        {// works if and only if player intends to talk to npc and they're close enough
+            if (!script.isTriggered)
+            {
+                script.dialogue = thoughts;
+                script.TriggerDialogue(); // Trigger Dialogue spoken
+                script.isTriggered = true;
+            }
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
