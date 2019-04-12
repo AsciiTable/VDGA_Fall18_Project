4f0c4ec8047adf1506a1fdb18b56b39bec5f7520
Jessica Wei <jessicawei9@gmail.com>
2019-04-12 16:15:09 -0700

    redoing speech bubbles using Dictionary

---
+++ VGDA_Fall18_Project/Assets/_Script/_UI/SpeechBubbleTriggerDictionary.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechBubbleTriggerDictionary : MonoBehaviour
+{
+    public GameObject Player;
+    public float buffer = 5f;
+    public SpeechBubbleDictionary speechDict;
+    private Dictionary<float, string> dictionary;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int len = speechDict.checkpoints.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
