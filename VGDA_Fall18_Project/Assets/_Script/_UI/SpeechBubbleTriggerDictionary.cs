0f74498546f658658f2f361e04b18e5ad5be6026
Jessica Wei <jessicawei9@gmail.com>
2019-04-24 22:39:41 -0700

    Speech. Bubble. Works. AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- VGDA_Fall18_Project/Assets/_Script/_UI/SpeechBubbleTriggerDictionary.cs
+++ VGDA_Fall18_Project/Assets/_Script/_UI/SpeechBubbleTriggerDictionary.cs
@@ -13,45 +13,48 @@ public class SpeechBubbleTriggerDictionary : MonoBehaviour
     private bool alreadyOpen = false;
     public Animator anim;
     public Text speechText;
+    private int save;
 
     void Start()
     {
-        Debug.Log("Player Position Start: " + Player.position.x);
+        //Debug.Log("Player Position Start: " + Player.position.x);
         len = speechDict.checkpoints.Length;
         for (int i = 0; i < len; i++) {
             diction.Add(speechDict.checkpoints[i], speechDict.sentences[i]);
         }
+        save = -9999;
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        Debug.Log("Player Position: " + Player.position.x);
-        for (int i = 0; i < len; i++)
-        {
-            if (!alreadyOpen)
-            {
+        Debug.Log("ANIM ON UPDATE: " + anim.GetBool("isOpen"));
+        //Debug.Log("Player Position: " + Player.position.x);
+        if (!alreadyOpen) {
+            for (int i = 0; i < len; i++) {
                 if (Mathf.Abs(speechDict.checkpoints[i] - Player.position.x) < buffer)
                 {
-                   
+                    save = i;
                     TriggerSpeech(diction[speechDict.checkpoints[i]]);
-                    alreadyOpen = true;
+                    break;
                 }
             }
-            else {
-                if (Mathf.Abs(speechDict.checkpoints[i] - Player.position.x) > buffer)
-                {
-                    alreadyOpen = false;
-                    anim.SetBool("isOpen", false);
-                }
+        }
+        else
+        {
+            if (Mathf.Abs(speechDict.checkpoints[save] - Player.position.x) > buffer)
+            {
+                alreadyOpen = false;
+                anim.SetBool("isOpen", false);
+                save = -9999;
             }
         }
     }
 
     public void TriggerSpeech(string speech) {
-        if (!alreadyOpen) {
-            speechText.text = "";
-            anim.SetBool("isOpen", true);
-        }
         speechText.text = speech;
+        anim.SetBool("isOpen", true);
+        Debug.Log("ANIM: " + anim.GetBool("isOpen"));
+        alreadyOpen = true;
+        Debug.Log("Text: " + speech);
     }
 }
