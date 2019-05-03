4fe09e931f43654f46bf6a19f8a7db86a55a364c
jusin-PC\Just <justincheung6@gmail.com>
2019-05-03 09:14:18 -0700

    Patched up cameras, and new Checkpoints disappear visually after touching one

--- VGDA_Fall18_Project/Assets/_Script/CheckpointSet.cs
+++ VGDA_Fall18_Project/Assets/_Script/CheckpointSet.cs
@@ -4,29 +4,33 @@ using UnityEngine;
 
 public class CheckpointSet : MonoBehaviour
 {
+    private SpriteRenderer sprite;
     private Player player;
     private IsCheckpoint isCheck;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("player").GetComponent<Player>();
+        sprite = GetComponent<SpriteRenderer>();
+        player = GameObject.Find("Player").GetComponent<Player>();
+        isCheck = (IsCheckpoint)FindObjectOfType(typeof(IsCheckpoint));
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        if (isCheck.checkpoint)
+            sprite.enabled = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            isCheck = GameObject.Find("UndyingScriptHolder").GetComponent<IsCheckpoint>();
-            isCheck.checkpoint = true;
             isCheck.pointX = gameObject.transform.position.x;
             isCheck.pointY = gameObject.transform.position.y;
+            isCheck.checkpoint = true;
+            Debug.Log("Checkpoint");
         }
     }
     }
